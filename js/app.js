367a627a00b4668e547ef2b555c405438b0f5321
Morella92 <morena.piem@outlook.com>
2023-02-06 18:10:54 +0100

    Change click order

--- js/app.js
+++ js/app.js
@@ -15,12 +15,20 @@ console.log(slides)
 const carousel = document.createElement('div')
 console.log(carousel)
 
+const rightArrow = document.querySelector('.arrow-right')
+console.log(rightArrow)
+
+const leftArrow = document.querySelector('.arrow-left')
+console.log(leftArrow)
+
+let activeSlide = 0
+
 for (let i = 0; i < slides.length; i++){
 
     let citySlides = document.createElement('div')
     console.log(citySlides)
 
-    citySlides.classList.add('.slide.active')
+    citySlides.classList.add('.slide')
 
     let slideContent = document.createElement('img')
     console.log(slideContent)
@@ -30,35 +38,31 @@ for (let i = 0; i < slides.length; i++){
     citySlides.appendChild(slideContent)
 
     carousel.appendChild(citySlides)
+
+    rightArrow.addEventListener('click', function(){
+        console.log('click')
+    
+        let currentSlide = citySlides [activeSlide] 
+        console.log (currentSlide)
+        
+        currentSlide.classList.remove('active')
+    
+        activeSlide = activeSlide + 1
+    
+        let nextSlide = citySlides[activeSlide]
+        console.log(nextSlide)
+    
+        nextSlide.classList.add('active')
+    
+    })
 }
 
-document.body.appendChild(carousel)
+document.body.prepend(carousel)
 
 
-const rightArrow = document.querySelector('.arrow-right')
-console.log(rightArrow)
 
-const leftArrow = document.querySelector('.arrow-left')
-console.log(leftArrow)
 
-let activeSlide = 0
 
-rightArrow.addEventListener('click', function(){
-    console.log('click')
-
-    let currentSlide = slideCities [activeSlide] 
-    console.log (currentSlide)
-
-    currentSlide.classList.remove('active')
-
-    activeSlide = activeSlide + 1
-
-    let nextSlide = slideCities[activeSlide]
-    console.log(nextSlide)
-
-    nextSlide.classList.add('active')
-
-})
 
 leftArrow.addEventListener('click', function(){
     console.log('click')
